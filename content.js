74e487377774e48d833f6f7e08ac6c095947e0f2
bluer222 <samuelzhimerlin@gmail.com>
2024-11-14 05:45:59 -0500

    listen to changes and insert player if necessary

--- content.js
+++ content.js
@@ -1,10 +1,20 @@
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(insertPlayer);
 
-// Wait for the page load completely before clicking
-window.addEventListener('load', () => {
-    const videoId = window.location.search.split("&")[0].substring(3);
+// Start observing the target node for configured mutations
+observer.observe(document.body, { attributes: false, childList: true });
 
-    const newPlayer = document.createElement("iframe");
-newPlayer.src = "https://www.youtube.com/embed/" + videoId;
-newPlayer.style = "width:100%; height:100%;"
-document.getElementById("movie_player").replaceWith(newPlayer);
-});
+function insertPlayer() {
+    console.log("change");
+    //if there is a video player then replace it
+        if (document.getElementById("movie_player") !== null) {
+            // Wait for the page load completely before switching it out
+            const videoId = window.location.search.split("&")[0].substring(3);
+
+            const newPlayer = document.createElement("iframe");
+            newPlayer.id = "newPlayer"
+            newPlayer.src = "https://www.youtube.com/embed/" + videoId;
+            newPlayer.style = "width:100%; height:100%;"
+            document.getElementById("movie_player").replaceWith(newPlayer);
+        }
+}
